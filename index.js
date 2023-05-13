c234d1ed755a2d87eae96b11a87d25872d3345c4
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-13 23:36:23 +0600

    JWT function added

--- index.js
+++ index.js
@@ -20,6 +20,27 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "You are not authorized" });
+  } else {
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ error: true, message: "You are not authorized" });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -57,8 +78,13 @@ async function run() {
 
     // Booking
 
-    app.get("/bookings", async (req, res) => {
-      console.log(req.query.email);
+    app.get("/bookings", verifyJwt, async (req, res) => {
+      const decoded = req.decoded;
+      console.log("decoded", decoded);
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: "Access Forbidden" });
+      }
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
